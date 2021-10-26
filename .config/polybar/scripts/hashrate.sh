02282191f313bcc9a1020a38e69b6173d84173d4
Hazed SPaCEx <livewire192@gmail.com>
2021-10-26 16:28:53 -0700

    Fix hashrate script

--- .config/polybar/scripts/hashrate.sh
+++ .config/polybar/scripts/hashrate.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s http://127.0.0.1:4067/summary | python3 -c "import sys, json; data = json.load(sys.stdin); print(\"%.2f MH/s %iW\" % ((data['hashrate'] / 1000000), data['gpus'][0]['power']))" || echo "N/A"
+curl -s http://127.0.0.1:4067/summary | python -c "import sys,json; exec(\"try:data=json.load(sys.stdin);print('%.2f MH/s %iW'%(data['hashrate']/1000000,data['gpus'][0]['power']));\nexcept:print('N/A')\")"
